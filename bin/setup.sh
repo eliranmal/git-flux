23fb1b3649d42b8bb41fa140e15d4c371af2a164
eliranm <eliranm@liveperson.com>
2017-01-10 00:47:13 +0200

    alter bash setup to work with msys (mingw)

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,57 +1,65 @@
 #!/usr/bin/env bash
 
-if [ -z "$INSTALL_PREFIX" ] ; then
-	# currently supporting default prefix in common *nix systems, to support more flavors, use this: https://gist.github.com/eliranmal/567ecf4b40a4a5cc0218
+echo "- - - git-le setup - - -"
+
+if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "darwin"* ]]; then # linux / mac osx
 	INSTALL_PREFIX="/usr/local/bin"
+elif [[ "$OSTYPE" == "msys" ]]; then # mingw
+	INSTALL_PREFIX="$PROGRAMFILES/Git/usr/bin"
+fi
+
+if [ -z "$INSTALL_PREFIX" ] ; then
+	echo "could not resolve install path prefix. use the INSTALL_PREFIX environment variable to set it manually."
 fi
 
 if [ -z "$REPO_URL" ] ; then
 	REPO_URL="http://lpgithub.dev.lprnd.net/WebJedi/le-ui-gitflow.git"
 fi
 
-REPO_NAME="le-ui-gitflow"
-EXEC_FILES="git-le"
-SCRIPT_FILES="git-le-wat git-le-foo gitle-private"
-
-echo "### git-le setup ###"
+exec_files="git-le"
+script_files="git-le-wat git-le-foo gitle-private-cmd"
 
 case "$1" in
 	help)
 		echo "usage: [environment] setup.sh [install|uninstall]"
 		echo "environment:"
-		echo "   INSTALL_PREFIX=$INSTALL_PREFIX"
 		echo "   REPO_URL=$REPO_URL"
 		echo "   REPO_PATH=$REPO_PATH"
+		echo "   INSTALL_PREFIX=$INSTALL_PREFIX"
 		exit
 		;;
 	uninstall)
-		echo "uninstalling git-le from $INSTALL_PREFIX"
-		if [ -d "$INSTALL_PREFIX" ] ; then
-			for script_file in $SCRIPT_FILES $EXEC_FILES ; do
-				echo "rm -vf $INSTALL_PREFIX/$script_file"
-				rm -vf "$INSTALL_PREFIX/$script_file"
-			done
-		else
-			echo "the '$INSTALL_PREFIX' directory was not found."
-			echo "do you need to set INSTALL_PREFIX ?"
+		if [ ! -d "$INSTALL_PREFIX" ] ; then
+			echo "the '$INSTALL_PREFIX' directory was not found. use the INSTALL_PREFIX environment variable to set it manually."
+			exit
 		fi
+		echo "uninstalling git-le from $INSTALL_PREFIX"
+		for script_file in $script_files $exec_files ; do
+			echo "rm -vf $INSTALL_PREFIX/$script_file"
+			rm -vf "$INSTALL_PREFIX/$script_file"
+		done
 		exit
 		;;
 	*)
+		if [ ! -d "$INSTALL_PREFIX" ] ; then
+			echo "the '$INSTALL_PREFIX' directory was not found. use the INSTALL_PREFIX environment variable to set it manually."
+			exit
+		fi
 		echo "installing git-le to $INSTALL_PREFIX"
 		if [ -d "$REPO_PATH" -a -d "$REPO_PATH/.git" ] ; then
-			echo "Using existing repo: $REPO_PATH"
+			echo "using existing repo: $REPO_PATH"
 			repo_dir="$REPO_PATH"
 		else
-			echo "cloning repo from github to $REPO_NAME"
-			git clone "$REPO_URL" "$REPO_NAME"
-			repo_dir="$REPO_NAME"
+			repo_name="le-ui-gitflow"
+			echo "cloning repo from github to $repo_name"
+			git clone "$REPO_URL" "$repo_name"
+			repo_dir="$repo_name"
 		fi
 		install -v -d -m 0755 "$INSTALL_PREFIX"
-		for exec_file in $EXEC_FILES ; do
+		for exec_file in $exec_files ; do
 			install -v -m 0755 "$repo_dir/$exec_file" "$INSTALL_PREFIX"
 		done
-		for script_file in $SCRIPT_FILES ; do
+		for script_file in $script_files ; do
 			install -v -m 0644 "$repo_dir/commands/$script_file" "$INSTALL_PREFIX"
 		done
 		exit
