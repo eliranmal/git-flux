2e2736f1f76a9e0c3ea5ee8bd3ef6e82de47ce76
eliranm <eliranm@liveperson.com>
2017-02-05 19:21:22 +0200

    update text formatter: protect from 8-color terminals, improve ansi impl', added stub for markdown impl

--- bin/usage-gen.sh
+++ bin/usage-gen.sh
@@ -18,13 +18,23 @@ echo "" > $main
 raw_tput_bold="$(tput bold)"
 #printf 'raw_tput_bold: %s\n' $raw_tput_bold
 #echo "$raw_tput_bold"
-#printf 'raw_tput_value: %q\n' $raw_tput_bold
+printf 'raw_tput_bold: %q\n' $raw_tput_bold
+
+
+
 
 #tput_bold="$(printf '%q' "$(printf '%q' $raw_tput_bold | sed -e 's,[$'"'"'],,g')")"
 #tput_bold="$(printf '%q' $raw_tput_bold | sed -e 's,[$'"'"'],,g')"
-tput_bold="$(printf '%q' $raw_tput_bold | sed -e 's,[$'"'"'],,g' | sed -e 's,\[,\\&,g')"
 #tput_bold="$(printf '%q' $raw_tput_bold | sed -e 's,[$'"'"'],,g' | sed -e 's,[\[],\\&,g')"
-echo "$tput_bold"
+
+tput_bold="\x$(printf "$raw_tput_bold" | hexdump -e '1/1 "%x" 1/1 "%s"')"
+#tput_bold="\x$(printf "$raw_tput_bold" | hexdump -e '1/1 "%x" 1/1 "%_p"')"
+
+tput_bold="$(printf '%q' "$tput_bold")"
+#tput_bold="\x$tput_bold"
+
+echo "tput_bold: $tput_bold"
+
 
 #echo "$(git flux init -h | sed -e 's,'"$(printf '%q' $tput_bold)"',A,g')" >> $main
 echo "$(git flux init -h | sed -e 's,'"$tput_bold"',A,g')" >> $main
