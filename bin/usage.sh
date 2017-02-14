61314b67a7a7e22fa67de755c0ac00fe215283da
eliranm <eliranm@liveperson.com>
2017-02-14 02:11:26 +0200

    refactor: encapsulation

--- bin/usage.sh
+++ bin/usage.sh
@@ -2,34 +2,43 @@
 
 
 main() {
-	
-	local source_dir=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-	
-	local output_format="markdown"
-	local output_dir="$source_dir/../usage"
-	local file_prefix="git-flux-"
+	local output_dir="$( source_dir )/../usage"
 
-	if [[ -d ${output_dir} ]]; then
-		rm -rf ${output_dir}
+	clean_dir "$output_dir"
+	generate_dox "$output_dir"
+}
+
+source_dir() {
+	printf "%s" "$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
+}
+
+clean_dir() {
+	if [[ -d $1 ]]; then
+		rm -rf $1
 	fi
-	mkdir -p ${output_dir}
+	mkdir -p $1
+}
 
+generate_dox() {
+	local output_dir="$1"
+	local output_format="markdown"
+	local file_prefix="git-flux-"
 	# generate the main file dox (git-flux)
 	env DOX_RENDER=true \
-		FORMAT=${output_format} \
-		OUTPUT_DIR=${output_dir} \
+		FORMAT="$output_format" \
+		OUTPUT_DIR="$output_dir" \
 		OUTPUT_FILE=main \
 		git flux -h
-	
+
 	# generate all other files dox (git-flux-*)
-	for path in ${source_dir}/../${file_prefix}*
+	for path in $( source_dir )/../${file_prefix}*
 	do
 		local cmd="${path##*/$file_prefix}"
 		env DOX_RENDER=true \
-			FORMAT=${output_format} \
-			OUTPUT_DIR=${output_dir} \
-			OUTPUT_FILE=${cmd} \
-			git flux ${cmd} -h
+			FORMAT="$output_format" \
+			OUTPUT_DIR="$output_dir" \
+			OUTPUT_FILE="$cmd" \
+			git flux "$cmd" -h
 	done
 }
 
