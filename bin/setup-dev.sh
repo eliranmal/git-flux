5921f0803bd52e31080abd17f603ae609951f716
eliranm <eliranm@liveperson.com>
2017-03-28 01:58:03 +0300

    testing pre-commit hook setup

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -3,6 +3,7 @@
 
 # activates the /bin/setup.sh script with this repo's path, to enable installing from source
 
+
 main() {
 	local source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 	local root_dir="$source_dir/.."
