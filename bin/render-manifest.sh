de6fd698015c2f8b4968391b7c3c64da994b1a45
eliranm <eliranm@liveperson.com>
2017-04-03 18:46:15 +0300

    playing with pre-commit

--- bin/render-manifest.sh
+++ bin/render-manifest.sh
@@ -7,10 +7,7 @@ main() {
 	ensure_template_file
 	ensure_output_file
 	
-	local commit_message="$(get_commit_message)"
-	
 	render_manifest_template
-	publish_manifest "$commit_message"
 }
 
 ensure_template_file() {
@@ -22,10 +19,6 @@ ensure_output_file() {
 	OUTPUT_FILE="${OUTPUT_FILE:-${git_root}/MANIFEST}"
 }
 
-get_commit_message() {
-	[[ -f $OUTPUT_FILE ]] && { echo "update manifest"; } || { echo "add manifest file"; }
-}
-
 render_manifest_template() {
 	# declare manifest placeholders as variables
 	local version="$(cat ${source_dir}/../VERSION)"
@@ -35,19 +28,6 @@ render_manifest_template() {
 
 render_template() {
 	eval "echo \"$(cat $1)\""
-}
-
-publish_manifest() {
-	local commit_message="$1"
-	git add ${OUTPUT_FILE}
-	if has_changed "$OUTPUT_FILE"; then
-		git commit --only -m "$commit_message" -- ${OUTPUT_FILE}
-		git push origin
-	fi
-}
-
-has_changed() {
-	[[ $(git diff-index --name-only HEAD --) =~ $(basename "$1") ]]
 }
 
 
