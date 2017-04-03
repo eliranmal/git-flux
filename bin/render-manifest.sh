40ebd4d6cedca2717e31e7f80a2bcc62703e778e
eliranm <eliranm@liveperson.com>
2017-04-03 18:50:53 +0300

    playing with pre-commit

--- bin/render-manifest.sh
+++ bin/render-manifest.sh
@@ -5,8 +5,11 @@ main() {
 	local source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 	
 	ensure_template_file
+	log "template file: [$TEMPLATE_FILE]"
 	ensure_output_file
+	log "output file: [$OUTPUT_FILE]"
 	
+	log "render manifest file from template."
 	render_manifest_template
 }
 
@@ -20,14 +23,18 @@ ensure_output_file() {
 }
 
 render_manifest_template() {
-	# declare manifest placeholders as variables
+	log "declare manifest placeholders as variables."
 	local version="$(cat ${source_dir}/../VERSION)"
-	# inject values into placeholders and write to output file
+	log "inject values into placeholders and write to output file."
 	render_template ${TEMPLATE_FILE} > ${OUTPUT_FILE}
 }
 
 render_template() {
 	eval "echo \"$(cat $1)\""
+}
+
+log() {
+	echo " [render-manifest] $1"
 }
 
 
