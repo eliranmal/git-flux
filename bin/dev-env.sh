0e5731efa8c0c0d18b0b8e1e1a333ae9faebe27d
eliranm <eliranm@liveperson.com>
2017-04-05 15:59:23 +0300

    lint dev-env script

--- bin/dev-env.sh
+++ bin/dev-env.sh
@@ -9,8 +9,12 @@
 #
 
 main() {
-	local source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
-	local git_root="$(git rev-parse --show-toplevel)"
+	local source_dir
+	local git_root
+	
+	# shellcheck disable=SC2164,SC2128
+	source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
+	git_root="$(git rev-parse --show-toplevel)"
 	if [[ $1 = nohooks ]]; then
 		log "removing git hooks from the repository"
 		unhookup || { exit 1; }
@@ -24,12 +28,12 @@ main() {
 
 # symlinks everything in the /bin/hooks directory to /.git/hooks
 hookup() {
-	ln -sfv ${source_dir}/hooks/* ${git_root}/.git/hooks
+	ln -sfv "$source_dir"/hooks/* "$git_root"/.git/hooks
 }
 
 unhookup() {
 	for file in ${source_dir}/hooks/*; do
-		rm -fv ${git_root}/.git/hooks/"$(basename "$file")"
+		rm -fv "$git_root"/.git/hooks/"$(basename "$file")"
 	done
 }
 
